3896988e0fa7f89324649797b76b49ea4fc6aab1
hadaskedar2020 <70915466+hadaskedar2020@users.noreply.github.com>
2020-12-09 20:19:36 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import datetime
-import logging
 
 from flask import Flask, render_template, request
 from sqlalchemy.exc import IntegrityError
@@ -12,7 +11,6 @@ DATABASE = r"sqlite:///zumba.db"
 app = Flask(__name__)
 
 engine = create_engine(DATABASE)
-# Base.prepare(engine, reflect=True)
 metadata = MetaData()
 metadata.reflect(engine, only=['genres', 'performers', 'songs', 'songs_performers'])
 Base = automap_base(metadata=metadata)
@@ -27,7 +25,7 @@ def select_basic_data_from_table(table_name):
     session = Session(engine)
     Table = getattr(Base.classes, table_name)
     all_data = session.query(Table.name).order_by(Table.name).all()
-    logging.critical(f"select_basic_data_from_table - {all_data}")
+    # logging.critical(f"select_basic_data_from_table - {all_data}")
     session.close()
     return all_data
 
@@ -39,10 +37,10 @@ def insert_simple_data(table_name, object_name):
     try:
         session.add(newrow)
         session.commit()
-        output_message = f"- {object_name} inserted successfully"
+        output_message = f"--- {object_name} inserted successfully"
     except IntegrityError:
         session.rollback()
-        output_message = f"- Error while trying to insert {object_name} - already exists."
+        output_message = f"--- Error while trying to insert {object_name} - already exists."
     session.close()
     return output_message
 
@@ -51,9 +49,9 @@ def get_table_id_and_description(table_name, exclude_value=None):
     session = Session(engine)
     Table = getattr(Base.classes, table_name)
     if exclude_value is not None:
-        all_data = session.query(Table.id, Table.name).filter(Table.id != exclude_value).all()
+        all_data = session.query(Table.id, Table.name).filter(Table.id != exclude_value).order_by(Table.name).all()
     else:
-        all_data = session.query(Table.id, Table.name).all()
+        all_data = session.query(Table.id, Table.name).order_by(Table.name).all()
     session.close()
     return all_data
 
@@ -70,10 +68,8 @@ def select_one_songs_data(song_id):
     session = Session(engine)
     song_data = session.query(Song.id, Song.name, Song.length_seconds, Song.length_seconds, Genre.name, Song.genre_id,
                               Song.link).filter_by(id=song_id).join('genres').all()
-    logging.critical(f"select_one_songs_data - {song_data}")
     song_performers = session.query(Song_performers.id, Song_performers.performer_id, Performer.name).filter_by(
         song_id=song_id).join('performers').all()
-    logging.critical(f"song_performers - {song_performers}")
     session.close()
     return song_data, song_performers
 
@@ -96,7 +92,7 @@ def update_or_insert_song(mode, song_id, song_name, genre_id, length_seconds, li
                 {"name": song_name, "genre_id": genre_id, "length_seconds": length_seconds, "link": link})
             session.commit()
         except IntegrityError:
-            logging.critical(f"update_or_insert_song IntegrityError")
+            # logging.critical(f"update_or_insert_song IntegrityError")
             session.rollback()
             status = 1
     if mode == 'I':
@@ -107,7 +103,7 @@ def update_or_insert_song(mode, song_id, song_name, genre_id, length_seconds, li
             session.refresh(song)
             song_id = song.id
         except IntegrityError:
-            logging.critical(f"update_or_insert_song IntegrityError")
+            # logging.critical(f"update_or_insert_song IntegrityError")
             session.rollback()
             status = 1
     if add_performer != 'None' and status == 0:
@@ -126,19 +122,18 @@ def search_songs_by_params(genre_id, performer_id):
     session = Session(engine)
     all_data = ""
     if genre_id != "None" and performer_id == "None":
-        logging.critical(f"search by genre")
         all_data = session.query(Song.id, Song.name, Song.length_seconds, Genre.name, Song.genre_id).filter_by(genre_id=genre_id).join(Genre).order_by(Song.name).all()
-        logging.critical(f"found_songs - {all_data}")
     if genre_id == "None" and performer_id != "None":
-        logging.critical(f"search by performer")
         all_data = session.query(Song.id, Song.name, Song.length_seconds, Genre.name, Song.genre_id).join(Genre).join(Song_performers).filter_by(performer_id=performer_id).order_by(Song.name).all()
-        logging.critical(f"found_songs - {all_data}")
     if genre_id != "None" and performer_id != "None":
-        logging.critical(f"search by performer and genre")
         all_data = session.query(Song.id, Song.name, Song.length_seconds, Genre.name, Song.genre_id).filter_by(genre_id=genre_id).join(Genre).join(Song_performers).filter_by(performer_id=performer_id).order_by(Song.name).all()
-        logging.critical(f"found_songs - {all_data}")
+    resultset = []
+    for row in all_data:
+        row = list(row)
+        row[2] = convert_length_seconds_into_minutes(row[2])
+        resultset.append(list(row))
     session.close()
-    return all_data
+    return resultset
 
 
 def convert_length_seconds_into_minutes(length_seconds):
@@ -225,11 +220,8 @@ def single_song_management(song_id=0):
 
 @app.route('/delete_song', methods=['POST'])
 def delete_song():
-    logging.critical(f"in delete_song")
     song_id = request.form['song_id']
-    logging.critical(f"song_id = {song_id}")
     delete_song_by_id(song_id)
-    logging.critical(f"after delete_song_by_id")
     all_data = select_songs_data_from_table()
 
     return render_template(
@@ -244,18 +236,15 @@ def delete_song():
 @app.route('/update_song', methods=['POST'])
 def update_song():
     song_id = request.form['song_id']
-    logging.critical(f"song_id: {song_id}")
     song_name = request.form['song_name']
     genre_id = request.form['genre_id']
     link = request.form['link']
     length_seconds = request.form['length_seconds']
     add_performer = request.form['add_performer_0']
-    logging.critical(f"add performer: {add_performer}")
     if song_id == 'None':
         mode = 'I'
     else:
         mode = 'U'
-    logging.critical(f"Mode: {mode}")
     status = update_or_insert_song(mode, song_id, song_name, genre_id, length_seconds, link, add_performer)
     if status == 0:
         all_data = select_songs_data_from_table()
@@ -270,7 +259,6 @@ def update_song():
         all_performes = get_table_id_and_description("performers")
         song_data, song_performers = select_one_songs_data(song_id)
         genres_data = get_table_id_and_description("genres", genre_id)
-        logging.critical(f"link: {link}")
         return render_template(
             'one_song_management.html',
             song_id=song_id,
@@ -290,20 +278,19 @@ def update_song():
 
 @app.route('/song_searcher', methods=['GET'])
 def song_searcher():
-    logging.critical(f"in song_searcher")
     display_message = ""
     found_data = ""
     mode = request.args.get('mode')
     genre_id = request.args.get('genre_id')
     performer_id = request.args.get('performer_id')
-    logging.critical(f"b4 data get, {genre_id}: genre_id")
     all_performes = get_table_id_and_description("performers")
     genres_data = get_table_id_and_description("genres")
-    logging.critical(f"after data get")
     if mode == "Search" and performer_id == "None" and genre_id == "None":
-       display_message = "- Choose at least 1 search criteria"
+       display_message = "--- Choose at least 1 search criteria"
     else:
-       found_data = search_songs_by_params(genre_id, performer_id)
+        found_data = search_songs_by_params(genre_id, performer_id)
+        if mode == "Search" and len(found_data) == 0:
+            display_message = "--- No matching songs found."
 
     return render_template(
         'search_songs.html',
